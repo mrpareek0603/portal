dd9ea7a1d1f08b4eade14475f67c1ade64773c72
mrpareek0603 <pareekrohan.rp@gmail.com>
2021-01-06 18:11:51 +0530

    changes in MentorInfo component

--- src/components/MentorInfo/MentorInfo.js
+++ src/components/MentorInfo/MentorInfo.js
@@ -18,13 +18,13 @@ function MentorInfo() {
                     <div id="Mentor1">
                         
                         {/* mentor pic */}
-                        <div id="Mentor1Image" className="Mentor1Image">
-                            <img width="100px" src="./logo.png" ></img>
+                        <div id="Mentor1Image" /*className="Mentor1Image"*/>
+                            <img className="MentorImage" width="100px" src="./logo.png" ></img>
                         </div>
                         {/* mentor description */}
                         <div id="Mentor1Name&Info">
                             <h3 className="MentorName">Pradeep Karmakar</h3>
-                            <p className="MentorInfo" >Pradeep has an experience of 13 yeaars of teaching 10,000+ students for their JEE Mains, JEE Advance and XII Board Examination. 
+                            <p className="MentorInfo" >Mr. Pradeep has an experience of 13 yeaars of teaching 10,000+ students for their JEE Mains, JEE Advance and XII Board Examination. 
                             <br></br>
                             <br></br>
                             He is a Bsc.( phy hon. + maths)graduate from RU and holds a PGD in Engineering from NIFFT and has worked at Birla Institute and STC(Nagpur) as an IIT JEE/Engineering/10+2 maths faculty.
@@ -39,16 +39,19 @@ function MentorInfo() {
                     {/* mentor 2 */}
                     <div id="Mentor2">
                         
-                    <div id="Mentor1Image" className="Mentor1Image">
-                            <img width="100px" src="./logo.png" ></img>
+                    <div id="Mentor2Image" className="Mentor1Image">
+                            <img className="Mentor1Image" width="100px" src="./Sohan Chaudhary.jpeg" ></img>
                         </div>
                         {/* mentor description */}
                         <div id="Mentor1Name&Info">
-                            <h3 className="MentorName">Vivekanand Vivek</h3>
-                            <p className="MentorInfo" >Vivek has an experience of teaching 10,000+ students for their Interview Preparation. He is an IIT BHU Graduate and has worked at Amazon for three years.
-                                <br></br>
-                                <br></br>
-                            He has in-depth knowledge of Data Structures & Algorithms. Many of his students now work at Amazon, Microsoft, Paytm, Directi, Razorpay and many more companies.
+                            <h3 className="MentorName">Sohan Chaudhary</h3>
+                            <p className="MentorInfo" >Mr. Sohan is a Chemistry Wizard and helpig students from past 10 years to crack India's toughest entrance examinations.
+                            <br></br>
+                            <br></br>
+                            {/* He is a Bsc.( phy hon. + maths)graduate from RU and holds a PGD in Engineering from NIFFT and has worked at Birla Institute and STC(Nagpur) as an IIT JEE/Engineering/10+2 maths faculty. */}
+                                {/* <br></br>
+                                <br></br> */}
+                            He has in-depth knowledge of Chemistry, and is reflected in his teachings, and his way of explaiing even the toughest concepts to the students, is something which helps his students scoring best in the exams.
                             </p>
                         </div>
                     
